d0ef1416974b1b258858bb5a0d61c5e94d7e6b8b
Eli Perkins <eli.j.perkins@gmail.com>
2020-06-23 14:06:43 -0400

    Revert "Bump to Swift 5"
    This reverts commit c7df99ea3b27ac478b26243492d2822716e08a02.

--- Package.swift
+++ Package.swift
@@ -1,15 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SQLite.swift",
     products: [.library(name: "SQLite", targets: ["SQLite"])],
     targets: [
-        .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
+        .target(name: "SQLite", dependencies: ["SQLiteObjc"], exclude: ["Info.plist"]),
         .target(name: "SQLiteObjc"),
-        .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
+        .testTarget(
+            name: "SQLiteTests", 
+            dependencies: ["SQLite"], 
+            path: "Tests/SQLiteTests", 
+            exclude: ["Info.plist", "fixtures/encrypted-3.x.sqlite", "fixtures/encrypted-4.x.sqlite"]
+        )
     ],
-    swiftLanguageVersions: [4, 5]
+    swiftLanguageVersions: [.v4, .v5]
 )
 
 #if os(Linux)
